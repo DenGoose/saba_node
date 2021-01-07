2b5a59ae2f04cbc5f165e1865500d53baa245a87
Dennis <d97510795@gmail.com>
2021-01-07 07:48:13 +0300

    2 часть

---
+++ web/script.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('form')
+const key = document.getElementById('key')
+const word = document.getElementById('word')
+const out = document.getElementById('out')
+
+function caesar(word, amount) {
+    if (amount < 0) {
+        return caesar(word, amount + 26);
+    }
+    let output = "";
+    for (let i = 0; i < word.length; i++) {
+        let c = word[i];
+        if (c.match(/[a-z]/i)) {
+            let code = word.charCodeAt(i);
+            if (code >= 65 && code <= 90) {
+                c = String.fromCharCode(((code - 65 + Number(amount)) % 26) + 65);
+            } else if (code >= 97 && code <= 122) {
+                c = String.fromCharCode(((code - 97 + Number(amount)) % 26) + 97);
+            }
+        }
+        output += c;
+        console.log(amount + " key");
+        console.log(word[i].charCodeAt());
+        console.log('-');
+        console.log(c.charCodeAt());
+    }
+    return output;
+};
+
+form.addEventListener('submit', (e) => {
+    // console.log(caesar(key.value, word.value))
+    out.innerHTML = caesar(key.value, word.value)
+    // out.innerHTML = 'qwe'
+    e.preventDefault()
+})
